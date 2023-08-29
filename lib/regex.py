597a0338a8b5a4f94e38393efeb8c4af07b75124
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-08-29 11:36:00 +0300

    Complete assignment

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,11 @@
 import re
 
-my_pattern = r""
-my_regex = re.compile(my_pattern)
+# Define your regular expression here
+my_regex_pattern = r"(It's such a lovely day today\.|Some weather we're having today, huh\?|Maybe today's just not my day\.)"
+my_regex = re.compile(my_regex_pattern)
 
+def fullmatch(string):
+    return my_regex.fullmatch(string)
+
+def findall(string):
+    return my_regex.findall(string)
